990a79d6bfb7158723a000b29ed61636739440e1
Gabriele Callegari <g.callegari@itsrizzoli.it>
2022-05-26 09:59:58 +0200

    feat: renamed raised

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,7 +10,7 @@ public class App
     {
         Operation sum=(int x, int y) -> x+y;
         Operation subtraction=(int x, int y) -> x-y;
-        Operation number_at=(int x, int y)->{
+        Operation raised=(int x, int y)->{
             int a=x;
             for (int i = 0; i < y; i++) {
                 a=a*x;
@@ -19,7 +19,7 @@ public class App
         };
         System.out.println("300+200="+DoOperation(300,200,sum));
         System.out.println("300+200="+DoOperation(300,200,subtraction));
-        System.out.println("3^5="+DoOperation(3,5,number_at));
+        System.out.println("3^5="+DoOperation(3,5,raised));
     }
     static int DoOperation(int x, int y, Operation op){
         return op.operation(x,y);
