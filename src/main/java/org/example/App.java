f4a781a20cf0d711844f9e07b079d29fb1b6a8d3
Gabriele Callegari <g.callegari@itsrizzoli.it>
2022-05-26 09:51:18 +0200

    feat: lambda

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+/**
+ * Lambda example
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Operation sum=(int x, int y) -> x+y;
+        Operation subtraction=(int x, int y) -> x-y;
+        Operation number_at=(int x, int y)->{
+            int a=x;
+            for (int i = 0; i < y; i++) {
+                a=a*x;
+            }
+            return a;
+        };
+        System.out.println("300+200="+DoOperation(300,200,sum));
+        System.out.println("300+200="+DoOperation(300,200,subtraction));
+        System.out.println("3^5="+DoOperation(3,5,number_at));
+    }
+    static int DoOperation(int x, int y, Operation op){
+        return op.operation(x,y);
+    }
+    interface Operation{
+        int operation(int x, int y);
+    }
+}
